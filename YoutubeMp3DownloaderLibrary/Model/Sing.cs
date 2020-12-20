02f95027adf7e46d97d9aa0976d636bd16b68a60
rutikeyone <rutikeyone@gmail.com>
2020-12-20 22:40:19 +0700

    Initial project and add files

---
+++ YoutubeMp3DownloaderLibrary/Model/Sing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeMp3DownloaderLibrary.Model
+{
+    public class Sing
+    {
+        public string GetInitial()
+        {
+            return "Вставьте ссылку и нажмите на кнопку для начала загрузки видео";
+        }
+    }
+}
---
+++ YoutubeMp3DownloaderLibrary/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using YoutubeMp3DownloaderLibrary.Model;
+
+namespace YoutubeMp3DownloaderLibrary.ViewModel
+{
+    public class MainViewModel
+    {
+        private Sing Sing;
+        public string SingText { get; set; }
+
+        public MainViewModel()
+        {
+            Sing = new();
+            SingText = Sing.GetInitial();
+        }
+    }
+}
