044d9b427ca3f1a80c3074b8feec3316ba01fc7f
rutikeyone <rutikeyone@gmail.com>
2020-12-21 23:21:32 +0700

    Update MainWindow

---
+++ YoutubeMp3DownloaderLibrary/Model/UI/UIClose.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows;
+
+namespace YoutubeMp3DownloaderLibrary.Model.UI
+{
+    public class UIClose
+    {
+        public void Close()
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
--- YoutubeMp3DownloaderLibrary/ViewModel/MainViewModel.cs
+++ YoutubeMp3DownloaderLibrary/ViewModel/MainViewModel.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using YoutubeMp3DownloaderLibrary.Model;
+using YoutubeMp3DownloaderLibrary.Model.UI;
 
 namespace YoutubeMp3DownloaderLibrary.ViewModel
 {
     public class MainViewModel
     {
+        private UIClose Close;
         private Sing Sing;
         public string SingText { get; set; }
 
         public MainViewModel()
         {
             Sing = new();
+            Close = new();
             SingText = Sing.GetInitial();
         }
+
+
+
     }
 }
